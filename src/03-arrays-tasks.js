c5d85700ab5740c1552ac5e197aeff1cbf791b87
Aliaksandr Rebkavets <aleksschpak@gmail.com>
2020-05-05 20:17:23 +0300

    feat: add solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -460,9 +460,12 @@ function getIdentityMatrix(/* n */) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
-}
+
+function getIntervalArray(start, end) { //
+  const arr = []; //
+  for (let i = start; i <= end; i += 1) { arr.push(i); } //
+  return arr; //
+} //* ****
 
 /**
  * Returns array containing only unique values from the specified array.
--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -43,9 +43,7 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
-}
+function getFactorial(n) { if (n === 1) { return 1; } return n * getFactorial(n - 1); }
 
 
 /**
@@ -60,8 +58,12 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let sum = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 
@@ -208,9 +210,7 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
-}
+function reverseString(str) { let arr = str.split(''); arr = arr.reverse(); return arr.join(''); }
 
 
 /**
@@ -225,8 +225,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const str = num.toString(); let revStr = ''; for (let i = str.length - 1; i >= 0; i -= 1) { revStr += str[i]; } return Number(revStr); //
 }
 
 
