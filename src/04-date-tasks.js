ad627679df26df37adc1877e2bec64b5a46997b4
Aliaksandr Rebkavets <aleksschpak@gmail.com>
2020-05-17 00:56:48 +0300

    feat: add solutions

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -53,8 +53,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(date) {
-  ;
+function isLeapYear(/* date */) {
+  throw new Error('Not implemented');
 }
 
 
